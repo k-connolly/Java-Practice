518146d9e127ef4fccdb5b711f71ddd992943080
k-connolly <49733412+k-connolly@users.noreply.github.com>
2020-11-10 12:07:38 -0600

    Java Practice Files

--- P15-Nest If Statements/src/javaClassNew.java
+++ P15-Nest If Statements/src/javaClassNew.java
@@ -1,26 +1,26 @@
 
 public class javaClassNew {
-	private static int userNum;
+	private static int inputNumber;
 	
 	public javaClassNew(int num) {
-		userNum = num;
+		inputNumber = num;
 	}
 	
 	public int getNum() {
-		return userNum;
+		return inputNumber;
 	}
 	
 	public void evaluation() {
 		//Evaluate the users number with if 
 		//and nested if statements
-		if (userNum < 10) {
-			System.out.println("Pass: " + userNum);
+		if (inputNumber < 10) {
+			System.out.println("Pass: " + inputNumber);
 		} else {
-			System.out.println("Fail: " + userNum);
-			if (userNum > 20) {
-				System.out.println("Nested Pass: " + userNum);
+			System.out.println("Fail: " + inputNumber);
+			if (inputNumber > 20) {
+				System.out.println("Nested Pass: " + inputNumber);
 			} else {
-						System.out.println("Nested Fail: " + userNum);
+						System.out.println("Nested Fail: " + inputNumber);
 			}
 		} 
 	}
