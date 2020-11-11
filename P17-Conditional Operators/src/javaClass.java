783409f1e6d538973c28417ec0213dafc1a4f794
k-connolly <49733412+k-connolly@users.noreply.github.com>
2020-11-10 21:11:06 -0600

    Java Practice Files

--- P17-Conditional Operators/src/javaClass.java
+++ P17-Conditional Operators/src/javaClass.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 public class javaClass {
-	private static int ifValue;
-	
+	private static int userNum;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("Enter your number: ");
-		ifValue = input.nextInt();
+		System.out.print("Enter your a number: ");
+		userNum = input.nextInt();
 		input.close();
 		
-		javaClassNew newObject = new javaClassNew(ifValue);
+		javaClassNew newObject = new javaClassNew(userNum);
 		newObject.message();
 	}
 
